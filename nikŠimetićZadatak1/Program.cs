deefd1fc9570a5c79200e253ca14dc9c011c98e6
niksimetic <55134764+niksimetic@users.noreply.github.com>
2020-02-06 11:02:57 +0100

    Add files via upload

---
+++ nikŠimetićZadatak1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nikŠimetićZadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, c, prosjek;
+            Console.WriteLine("Unesi 3 broja: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            prosjek = (a + b + c) / 3;
+
+
+                Console.WriteLine("Prosjek je:" + prosjek);
+            Console.ReadKey();
+
+        }
+    }
+}
